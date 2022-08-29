3ab548e4302206037a6cadf647473f3137435688
Sandoerton <sandoerton@gmail.com>
2022-08-29 17:09:49 -0300

    feat: models BlogPost

---
+++ src/database/models/blogPost.js
@@ -0,0 +1,20 @@
+const BlogPost = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define("BlogPost", {
+    id: DataTypes.INTEGER,
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: DataTypes.INTEGER,
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE
+  }, {
+    timestamps: false
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, { as: 'user', foreignKey: 'userId' });
+  };
+
+  return BlogPost;
+};
+
+module.exports = BlogPost;
