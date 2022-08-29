bdc144c2d9f045ceda3bf0f1bb9130d227282150
Sandoerton <sandoerton@gmail.com>
2022-08-29 16:38:38 -0300

    feat: models Category

--- src/database/models/category.js
+++ src/database/models/category.js
@@ -1,23 +1,12 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class Category extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  Category.init({
+const Category = (sequelize, DataTypes) => {
+  const Category = sequelize.define("Category", {
+    id: DataTypes.INTEGER,
     name: DataTypes.STRING
   }, {
-    sequelize,
-    modelName: 'Category',
+    timestamps: false
   });
+
   return Category;
 };
+
+module.exports = Category;
