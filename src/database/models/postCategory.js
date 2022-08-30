409a8ab8089a6d81cbbae8a10d9f50de3bbbe24a
Sandoerton <sandoerton@gmail.com>
2022-08-30 04:46:01 -0300

    feat: models PostCategory

---
+++ src/database/models/postCategory.js
@@ -0,0 +1,28 @@
+const PostCategory = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define("PostCategory", {
+    postId: DataTypes.INTEGER,
+    categoryId: DataTypes.INTEGER,
+  }, {
+    timestamps: false
+  });
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId'
+    });
+
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'posts',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'portId'
+    });
+  };
+
+  return PostCategory;
+};
+
+module.exports = PostCategory;
