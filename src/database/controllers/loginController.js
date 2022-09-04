ae8e2e52327f83101f68c579973c86d551bb9022
Sandoerton <sandoerton@gmail.com>
2022-09-04 10:41:56 -0300

    fix: create login with remove LoginService

--- src/database/controllers/loginController.js
+++ src/database/controllers/loginController.js
@@ -1,10 +1,14 @@
-const loginService = require('../services/loginService');
+const { tokenGenerate } = require('../helpers/token');
+const userService = require('../services/userService');
 
 const login = async (req, res) => {
   const { email, password } = req.body;
-  if (!email || !password) res.status(400).json({ message: 'Some required fields are missing' });
-  const token = await loginService.create({ email, password });
-  if (!token) return res.status(400).json({ message: 'Invalid fields' });
+  if (!email || !password) {
+    return res.status(400).json({ message: 'Some required fields are missing' });
+  }
+  const user = await userService.findUser({ email, password });
+  if (!user) return res.status(400).json({ message: 'Invalid fields' });
+  const token = tokenGenerate({ email });
   return res.status(200).json({ token });
 };
 
